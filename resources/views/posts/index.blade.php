6d4a3af9e580b3ddb3fba62452db27fb5be9049c
Kiernnn <paulosthant4644@gmail.com>
2024-09-24 16:23:38 +0630

    stash

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -33,9 +33,10 @@
                         <div class="friends mb-3">Search Result for " {{ $keyword }} "</div>
                         <div class="title-links">
                             {{-- <a href="{{ route('posts.search', $keyword) }}" class="links {{ Request::is('posts/search') ? 'active' : '' }}">{{ __('All') }}</a> --}}
-                            <a href="{{ route('posts.searchUsers', $keyword)}}"
+                            <a href="{{ route('posts.searchUsers', $keyword) }}"
                                 class="links {{ Request::is('posts/search-users') ? 'active' : '' }}">{{ __('Users') }}</a>
-                            <a href="{{ route('posts.searchPosts', $keyword) }}" class="links {{ Request::is('posts/search-posts') ? 'active' : '' }}">{{ __('Posts') }}</a>
+                            <a href="{{ route('posts.searchPosts', $keyword) }}"
+                                class="links {{ Request::is('posts/search-posts') ? 'active' : '' }}">{{ __('Posts') }}</a>
                         </div>
                     </div>
                 @endif
@@ -44,143 +45,6 @@
                     <div class="alert alert-danger">{{ session('error') }}</div>
                 @endif
 
-<<<<<<< HEAD
-                {{-- Search Results start --}}
-                @if (isset($keyword))
-                    @if (!$posts->isEmpty() || !$users->isEmpty())
-                        <div class="friends-container mb-3 mt-3">
-                            <div class="friends mb-3">Search Result for " {{ $keyword }} "</div>
-                            <div class="title-links">
-                                {{-- <a href="{{ route('posts.search', ['search' => $keyword]) }}" class="links {{ Request::is('posts/searchResults') ? 'active' : '' }}">{{ __('All') }}</a> --}}
-                                <a href="{{ route('posts.searchUsers', $keyword) }}"
-                                    class="links {{ Request::is('posts/search-users') ? 'active' : '' }}">{{ __('Users') }}</a>
-                                <a href="{{ route('posts.searchPosts', $keyword) }}"
-                                    class="links {{ Request::is('posts/search-posts') ? 'active' : '' }}">{{ __('Posts') }}</a>
-                            </div>
-                        </div>
-                    @endif
-                    <div class="scorll-search">
-                        {{-- search users --}}
-                        @if ($posts->isEmpty() && $users->isEmpty())
-                            <p style="color:white;">Not found.</p>
-                        @else
-                            @if (!$users->isEmpty())
-                                <h5>Users</h5>
-                                <div class="suggest-form-container mb-3">
-                                    <div class="search-profile mb-3">
-                                        @foreach ($users as $user)
-                                            @php
-                                                $postProfile = $user->profile;
-                                            @endphp
-                                            <div class="request-container mb-2">
-                                                <a href="{{ route('profile.show', $user->id) }}"
-                                                    class="searchProfile mb-0">
-                                                    @if ($postProfile && $postProfile->image && file_exists(public_path('profiles/' . $postProfile->image)))
-                                                        <img src="{{ asset('profiles/' . $postProfile->image) }}"
-                                                            alt="Profile Picture" class="profile-pic me-3">
-                                                    @else
-                                                        <img src="{{ asset('images/user_default.png') }}"
-                                                            alt="Profile Picture" class="profile-pic">
-                                                    @endif
-                                                    <div class="profile-info mb-0">
-                                                        <div class="profile-name">{{ $user->userName }}</div>
-                                                        <div id="successMessage{{ $user->id }}" class="add-fri"
-                                                            style="display: none; color: #808080; font-weight: bold;"></div>
-                                                    </div>
-                                                </a>
-                                            </div>
-                                        @endforeach
-                                    </div>
-                                </div>
-                            @endif
-                        @endif
-
-                        {{-- search posts --}}
-                        @if (!$posts->isEmpty())
-                            <h5>Posts</h5>
-                            @foreach ($posts as $post)
-                                @php
-                                    $postProfile = $post->user->profile;
-                                    $hasLoved = $post->loves()->where('user_id', auth()->id())->exists();
-                                @endphp
-                                <div class="post-container mb-3 shadow-sm p-4 rounded-4 text-light">
-                                    <a href="{{ route('profile.show', $post->user->id) }}" class="post-header d-flex">
-                                        @if ($postProfile && $postProfile->image && file_exists(public_path('profiles/' . $postProfile->image)))
-                                            <img src="{{ asset('profiles/' . $postProfile->image) }}" alt="Profile Picture"
-                                                class="profile-pic me-3">
-                                        @else
-                                            <img src="{{ asset('images/user_default.png') }}" alt="Profile Picture"
-                                                class="profile-pic">
-                                        @endif
-                                        <div class="profile-name">{{ $post->user->userName }}</div>
-                                        <div class="post-subtitle mb-2 small">{{ timeDiffInHours($post->created_at) }}
-                                        </div>
-
-                                        <p class="privacy">
-                                            <strong>
-                                                @if ($post->status === 'public')
-                                                    <i class="fas fa-globe" title="Public"></i>
-                                                @elseif($post->status === 'friends')
-                                                    <i class="fas fa-user-friends" title="Friends"></i>
-                                                @elseif($post->status === 'me')
-                                                    <i class="fas fa-lock" title="Me"></i>
-                                                @else
-                                                    <i class="fas fa-question" title="Unknown"></i>
-                                                @endif
-                                            </strong>
-                                        </p>
-                                    </a>
-
-                                    <div class="post-content mt-1">
-                                        <p class="content ml-10px">{{ $post->content }}</p>
-                                        @if ($post->image)
-                                            <img src="{{ asset('posts/' . $post->image) }}" class="post-image"
-                                                alt="Post image">
-                                        @endif
-                                        <div class="post-footer d-flex mt-1">
-                                            <form action="{{ route('posts.toggleLove', $post->id) }}" method="POST"
-                                                class="me-0" style="display:inline;">
-                                                @csrf
-                                                <button class="btn">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"
-                                                        fill="{{ $hasLoved ? '#dc3545' : '#fff' }}"
-                                                        class="bi {{ $hasLoved ? 'bi-heart-fill' : 'bi-heart' }}"
-                                                        viewBox="0 0 16 16">
-                                                        <path fill-rule="evenodd"
-                                                            d="{{ $hasLoved ? 'M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314' : 'm8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15' }}" />
-                                                    </svg>
-                                                </button>
-                                            </form>
-                                            @if ($post->loves->count())
-                                                <p class="love-count">{{ $post->loves->count() }}</p>
-                                            @else
-                                                <p hidden></p>
-                                            @endif
-                                            <button class="btn">
-                                                <a href="{{ route('posts.detail', $post->id) }}" class="card-link">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" height="20px"
-                                                        viewBox="0 -960 960 960" width="20px" fill="#fff">
-                                                        <path
-                                                            d="M880-80 720-240H160q-33 0-56.5-23.5T80-320v-480q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v720ZM160-320h594l46 45v-525H160v480Zm0 0v-480 480Z" />
-                                                    </svg>
-                                                </a>
-                                            </button>
-                                            @if ($post->comments->count())
-                                                <p class="cmt-count">{{ $post->comments->count() }}</p>
-                                            @else
-                                                <p hidden></p>
-                                            @endif
-                                        </div>
-                                    </div>
-                                </div>
-                            @endforeach
-                        @endif
-                    </div>
-                @endif
-                {{-- Search Results end --}}
-
-=======
->>>>>>> f105567ce79dffa7f56c7b645f51a1942adbfbbe
                 {{-- posts --}}
                 @foreach ($posts as $post)
                     @php
@@ -237,16 +101,16 @@
                                     </a>
                                     <div class="dropdown-menu dropdown-menu-end">
                                         <a href="{{ route('posts.edit', $post->id) }}" class="dropdown-item">
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="18px"
-                                                viewBox="0 -960 960 960" width="18px" fill="#fff">
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960"
+                                                width="18px" fill="#fff">
                                                 <path
                                                     d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
                                             </svg>
                                             {{ __('Edit post') }}
                                         </a>
                                         <a href="#" class="dropdown-item">
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="18px"
-                                                viewBox="0 -960 960 960" width="18px" fill="#fff">
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960"
+                                                width="18px" fill="#fff">
                                                 <path
                                                     d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm-40-82v-78q-33 0-56.5-23.5T360-320v-40L168-552q-3 18-5.5 36t-2.5 36q0 121 79.5 212T440-162Zm276-102q20-22 36-47.5t26.5-53q10.5-27.5 16-56.5t5.5-59q0-98-54.5-179T600-776v16q0 33-23.5 56.5T520-680h-80v80q0 17-11.5 28.5T400-560h-80v80h240q17 0 28.5 11.5T600-440v120h40q26 0 47 15.5t29 40.5Z" />
                                             </svg>
